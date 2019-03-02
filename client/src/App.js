2bce04a99353294f3834bb032a04f83f7989ccd6
Yobany <yobanydesign@gmail.com>
2019-03-02 11:46:05 -0500

    Added react router dependency and paths to App.js.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
 import Jumbotron from "./components/Jumbotron"
 import { Input, FormBtn } from "./components/Form";
@@ -6,26 +7,29 @@ import { Input, FormBtn } from "./components/Form";
 class App extends Component {
   render() {
     return (
+      <Router>
       <div>
         <Nav />
         <Jumbotron />
 
           <form className="formSection">
               <Input
-                // value={this.state.title}
-                // onChange={this.handleInputChange}
                 name="title"
                 placeholder="Title (required)"
               />
-              <FormBtn
-                // disabled={!(this.state.title)}
-                // onClick={this.handleFormSubmit}
-              >
+              <FormBtn>
                 Search
               </FormBtn>
           </form>
 
+          <Switch>
+            <Route exact path="/" component={Search} />
+            <Route exact path="/search" component={Search} />
+            <Route exact path="/saved" component={Saved} />
+            <Route exact path={NoMatch} />
+          </Switch>
       </div>
+      </Router>
       );
   }
 }
