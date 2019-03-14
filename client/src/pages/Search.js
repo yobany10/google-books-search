40f34cfde51753fa2a9d8b7633c42f8ff4666c86
Yobany <yobanydesign@gmail.com>
2019-03-13 21:05:27 -0400

    Added functions that will handle search input and submissions. Also added functions searching and saving books.

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -2,39 +2,63 @@ import React, { Component } from "react";
 import { Input, FormBtn } from "../components/Form";
 import BookList from "../components/BookList";
 import API from "../utils/API";
+import Book from "../components/Book";
 
 class Search extends Component {
     state = {
+        search: "",
         books: [],
-        title: "",
-        authors: "",
-        description: "",
-        image: "",
-        link: "",
-    };
+        currentList: "Results"
+    }
 
-    handleFormSubmit = event => {
+    searchBooks = () => {
+        API.searchBook("?q=" + this.state.search).then(res => {
+            var books = [];
+            res.data.forEach(i => {
+                var temp = {
+                    key: i.id,
+                    title: i.volumeInfo.title,
+                    authors: i.volumeInfo.authors,
+                    image: i.volumeInfo.thumbnail,
+                    link: i.volumeInfo.infoLink,
+                    description: i.volumeInfo.description
+                }
+                books.push(temp);
+            });
+            this.setState({books: books});
+        });
+    }
+
+    saveBook = book => {
+        API.saveBook({
+            title: book.title,
+            authors: book.authors,
+            link: book.link,
+            description: book.description,
+            image: book.image,
+            googleId: book.key
+        })
+        .then(res => book)
+        .catch(err => console.log(err));
+    }
+
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit = event => {
         event.preventDefault();
-        if (this.state.title) {
-            //
+        if (this.state.search) {
+            this.searchBooks();
         }
     }
 
 render() {
     return (
         <div>
-
-            <form className="formSection">
-              <Input
-                name="title"
-                placeholder="Title (required)"
-              />
-              <FormBtn>
-                Search
-              </FormBtn>
-            </form>
-
-
         </div>
         );
     }
