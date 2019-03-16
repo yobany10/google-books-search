fc3bec12dc52bd6de64e20da0cd67637d17ce6da
Yobany <yobanydesign@gmail.com>
2019-03-16 11:29:25 -0400

    Ran npm run build

---
+++ client/build/precache-manifest.10242506e53d8fa56d2a49fe4ae1f49c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d672f040abfa3c0ea28a",
+    "url": "/google-books-search/static/js/runtime~main.d672f040.js"
+  },
+  {
+    "revision": "26343d2f4f4c8dbff628",
+    "url": "/google-books-search/static/js/main.26343d2f.chunk.js"
+  },
+  {
+    "revision": "04fca29fdaf4f01508c8",
+    "url": "/google-books-search/static/js/2.04fca29f.chunk.js"
+  },
+  {
+    "revision": "26343d2f4f4c8dbff628",
+    "url": "/google-books-search/static/css/main.a175124d.chunk.css"
+  },
+  {
+    "revision": "7e142efbdd40c1aa170d48a7682ea10a",
+    "url": "/google-books-search/index.html"
+  }
+];
