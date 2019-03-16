5d9d2c4eda88be633c63954860571d08d29c1c8d
Yobany <yobanydesign@gmail.com>
2019-03-16 13:04:17 -0400

    heroku update

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build/static/"));
   app.use(express.static("client/build/static/"));
+  app.use(express.static("build/static/"));
   app.use(express.static("client/build/"));
 }
 
