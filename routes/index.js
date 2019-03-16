144057a15024fc045975b35577818c86a1b497ba
Yobany <yobanydesign@gmail.com>
2019-03-16 12:44:24 -0400

    heroku update

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,9 @@ const apiRoutes = require("./api");
 router.use("/api", apiRoutes);
 
 // If no API routes are hit, send the React app
-router.use((req, res) =>
-  res.sendFile(path.join(__dirname, "../client/build/index.html"))
-);
-
+if (process.env.NODE_ENV === "develop") {
+  router.use((req, res) =>
+    res.sendFile(path.join(__dirname, "../client/build/index.html"))
+  );
+}
 module.exports = router;
