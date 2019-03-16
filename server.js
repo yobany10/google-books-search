a7d987791a4eb76c84709fdf59a20a3c9d0f6de3
Yobany <yobanydesign@gmail.com>
2019-03-16 12:15:25 -0400

    heroku update

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("/google-books-search/static/"));
+  app.use(express.static("/static/"));
 }
 
 // Add routes, both API and view
